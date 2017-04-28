12d82a2876a59f730c63a81ea3db1040ef5b26dd
Frederic Arnold <frederic.arnold@tu-dortmund.de>
2017-04-28 13:11:27 +0200

    insertionSort implementiert

--- Sortierung.java
+++ Sortierung.java
@@ -2,7 +2,18 @@ public class Sortierung
 {
     public Sortierung() {}
     public static void main(String []args) {}
-    public static void insertionSort(int []array) {}
+    public static void insertionSort(int []array)
+    {
+        for(int j = 1; j < array.length; j++) {
+            int key = array[j];
+            int i = j-1;
+            while((i >= 0) && array[i] > key) {
+                array[i+1] = array[i];
+                i--;
+            }
+            array[i+1] = key;
+        }
+    }
     public static boolean isSorted(int []array)
     {
         if(array.length == 0)
