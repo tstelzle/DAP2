67ed9a84fd2ec9c5f283f9b1b44ac9297eb8080e
tstelzle <tarek.stelzle@tu-dortmund.de>
2017-05-17 12:06:12 +0200

    Methodenimplementierung angefangen

--- ConvexHull.java
+++ ConvexHull.java
@@ -1,4 +1,4 @@
-import java.util.LinkedList<E>;
+import java.util.*;
 
 public class ConvexHull
 {
@@ -9,15 +9,49 @@ public class ConvexHull
 	
 	public static void main(String[] args)
 	{
-		LinkedList() liste = new LinkedList();
+		/*
+		LinkedList<Point> liste = new LinkedList<Point>();
+		liste.add(new Point(2, 3.0, 4.5));
+		System.out.println(liste.size());
+		*/
+	}
 
+	public LinkedList<Point> simpleConvex(Point[] ps)
+	{	
+		LinkedList<Point, Point> liste = new LinkedList<Point>();
+		Point eins,zwei;
+		for(p:ps)
+		{
+			for(q:ps)
+			{
+				boolean valid = true;
+				for(l:qs)
+				{
+					if(l != p && l != q)
+					{
+						
+					}
+				}
+				Point zwei = q;
+			}
+			Point eins = p;
 
-	public List<Point> simpleConvex(Point[] ps)
-	{
-		;
+		}
+		liste.add(eins, zwei);
+		return null;
 	}
 
 	Point[][] choose2(Point[] ps)
 	{
+		Point[][] arr2;
+		for(int i=0; i<ps.length; i++)
+		{
+			for(int u=1; u<ps.length; u++)
+			{
+				arr2[i][arr2 		
+			}
+		}
+		return arr2;
+
 	}
 }
