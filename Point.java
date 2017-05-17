f84b43c13e0bafaf314b38f60c201604fd99f29e
tstelzle <tarek.stelzle@tu-dortmund.de>
2017-05-17 14:13:55 +0200

    Kommentare hinzugefügt

--- Point.java
+++ Point.java
@@ -1,8 +1,13 @@
-public class Point
+//Autoren: Tarek Stelzle und Frederic Arnold
+
+//Die Klasse Point erstellt ein Objekt, welches eine Dimension und genuegend Koordinaten fuer die Dimension hat
+class Point
 {
+	//globale Variablen fuer das Objekt Point
 	int dim;
 	double[] ds;
 
+	//Konstruktur erzeugt das Objekt und setzt die globalen Variablen anhand er uebergebenen Parameter
 	public Point(int d, double... args)
 	{
 		if(d != args.length) {
@@ -12,6 +17,7 @@ public class Point
 		this.ds = args;
 	}
 
+	//Methode zur Ausgabe zur i-ten Koordinate
 	public double get(int i)
 	{
 		if(i >= this.ds.length || i < 0) {
@@ -20,6 +26,7 @@ public class Point
 		return this.ds[i];
 	}
 
+	//Methode zur Ausgabe der Dimension des Points
 	public int dim()
 	{
 		return this.dim;
