efa54ad4456c5b5a8e463a2ef5e7958ed48ed2bb
Tarek Stelzle <tstelzle@online.de>
2017-05-22 13:05:49 +0200

    Aufgabe 6.1

--- Anwendung.java
+++ Anwendung.java
@@ -1,9 +1,18 @@
+//Die Klasse berechnet das Scheduling fuer Prozess die in einer Textdatei gespeichert sind.
+
+//Autoren: Tarek Stelzle und Frederic Arnold
+
+//import fuer das Einlesen und Auslesen der Datei
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+
+//import fuer LinkedList
 import java.util.*;
 
+
+//Die Klasse Intervall erstellt ein Objekt welches die Dauer des Prozesses in einem Array angibt.
 class Intervall
 {
 	int[] bereich;
@@ -29,6 +38,7 @@ class Intervall
 		return ausgabe;
 	}
 
+	//Zweite Ausgabemethode - wird in diesem Programm nicht verwendet
 	public String toString2()
 	{
 		String ausgabe = new String("Intervallanfang: " + getStart() + " Intevallende: " + getEnd());
@@ -37,6 +47,7 @@ class Intervall
 }
 
 
+//Die Klasse implementiert den Algorithmus und organisiert das Einlesen und die Ausgabe
 public class Anwendung
 {
 	static String dateiName;
@@ -49,6 +60,7 @@ public class Anwendung
 
 	public static void main(String[] args)
 	{
+		//einlesen des Dateipfades und pruefen auf Korrektheit
 		String dateiPfad = "";
 		try{
 			dateiPfad = args[0];
@@ -63,21 +75,34 @@ public class Anwendung
 			System.out.println("Dateipfad existiert nicht.");
 			return;
 		}
-
+		
+		//Pruefen auf richte Argumenteanzahl
+		if(args.length > 1)
+		{
+			System.out.println("Zu viele Argumente eingegeben.");
+			return;
+		}
+		
+		//Fals try and catch fehlschlaegt wird hier das Programm abgebrochen (unnoetig)
 		if(dateiPfad.equals(""))
 		{
 			return;
 		}
 
+		//Speicher des Dateinamens
 		int index = dateiPfad.lastIndexOf('/');
 		dateiName = dateiPfad.substring(index+1, dateiPfad.length());
+		//Datei auslesen
 		Intervall[] auslesenArr = auslesen(dateiPfad);
-		
+		//Array sortieren
 		Intervall[] SortArr = sortEnd(auslesenArr);	
+		//Schedulingalgorithmus anwenden
 		Intervall[] ScheduleArr = intervallScheduling(SortArr);
+		//Berechnung ausgeben
 		ausgabe(auslesenArr, SortArr, ScheduleArr);
 	}
 
+	//Methode zur Ausgabe eines Array
 	public static void ArrAusgabe(Intervall[] arr)
 	{
 		System.out.print("[");
@@ -95,7 +120,8 @@ public class Anwendung
 		System.out.println("]");
 		
 	}
-
+	
+	//Methode zur Ausgabe, die in der Aufgabenstellung vorgegeben war
 	public static void ausgabe(Intervall[] auslesenArr, Intervall[] SortArr, Intervall[] ScheduleArr)
 	{
 		System.out.println("Bearbeite Datei \"" + dateiName + "\".");
@@ -110,22 +136,32 @@ public class Anwendung
 		ArrAusgabe(ScheduleArr);
 	}	
 
+	//Algorithmus zum Intervallscheduling, der in der Vorlesung vorgegeben wurde
 	public static Intervall[] intervallScheduling(Intervall[] intervalls)
 	{
+		//erstellen einer Liste, zur Speicherung der gewaehlten Prozesse (einfacher als Array)
 		LinkedList<Intervall> ausgabeList = new LinkedList<Intervall>();
+		//Abbruch fuer die Vorschleife
 		int n = intervalls.length;
+		//erstes Element wird in die Liste eingefuegt, da das Array schon sortiert ist
 		ausgabeList.add(intervalls[0]);
+		//Variable j wird initialisiert
 		int j=0;
 
+		//Die for-Schleife durchlauft das Array
 		for(int i=1; i< n; i++)
 		{
+			//Falls der Anfangswert des Vorgaengers groesser gleich dem Endwert des Nachfolgers ist, 
+			//wird der Prozess mit dem Anfangswert in die Liste aufgenommen
 			if(intervalls[i].getStart() >= intervalls[j].getEnd())
 			{
 				ausgabeList.add(intervalls[i]);
+				//j wird auf den Vorgaenger gesetzt
 				j=i;
 			}
 		}
 		
+		//Liste wird in ein Arrayuebertragen, dass dann zurueckgegeben wird
 		int k=0;
 		Intervall[] ausgabe = new Intervall[ausgabeList.size()];
 
@@ -138,6 +174,7 @@ public class Anwendung
 		return ausgabe;
 	}
 	
+	//Sortieren des Arrays mit Bubblesort
 	public static Intervall[] sortEnd(Intervall[] intervalls)
 	{
 		int k=intervalls.length;
@@ -158,30 +195,52 @@ public class Anwendung
 		return intervalls;
 	}
 
+	//Auslesend der Datei
 	public static Intervall[] auslesen(String dateipfad)
 	{
+		//Zeilensaehler (wichtig fuer die Ausgabe)
 		int count = 0;
 
+		//erstellen einer Liste fuer die ausgelesenen Zeilen
 		LinkedList<Intervall> liste = new LinkedList<Intervall>();
 	
+		//Ausleser initialiseren
 		BufferedReader in = null;
 		try{
+			//Versuch den Ausleser zu erstellen
 			in = new BufferedReader(new FileReader(dateipfad));
+			//Variable fuer die naechste Zeile initialisieren
 			String zeile = null;
+			//Auslesen der Zeilen nacheinander, solange bis einen null ist
 			while((zeile = in.readLine()) != null)
 			{
+				//Zeilenzaehler wird um eins erhoeht
 				count++;
+				//Initialisieren von 2 Strings
 				String Zeins = "";
 				String Zzwei = "";
 
 				int x = zeile.indexOf(",");
 				
+				//Initialisieren von 2 String, fuer den Anfangs- und Endwert des Prozess
 				Zeins = zeile.substring(0, x);
 				Zzwei = zeile.substring(x+1, zeile.length());
-
-				int Start = Integer.parseInt(Zeins);
-				int End = Integer.parseInt(Zzwei);
+				
+				int Start = 0;
+				int End = 0;
+				//Versuch die Strings in Ints umzuwandeln
+				try{
+					Start = Integer.parseInt(Zeins);
+					End = Integer.parseInt(Zzwei);
+				}catch (Exception e)
+				{
+					System.out.println("Hier sollte eine Zahl stehen.");
+					return null;
+				}
+				
+				//neues Intervall mit den 2 Ints erstellen
 				Intervall lesen = new Intervall(Start, End);
+				//Intervall der Liste hinzufuegen
 				liste.add(lesen);
 			}	
 		}catch(IOException e){
@@ -189,6 +248,7 @@ public class Anwendung
 		}
 		finally
 		{
+			//insofern der Ausleser noch nicht geschlossen wurde, wird er hier geschlossen
 			if(in != null)
 			{
 				try{
@@ -200,6 +260,8 @@ public class Anwendung
 			}
 		}
 			
+
+		//Umschreiben der Liste in ein Array
 		Intervall[] ausgabe = new Intervall[liste.size()];
 
 		int i=0;
@@ -209,7 +271,9 @@ public class Anwendung
 			i++;
 		}
 		
+		//globale Variable auf Zeilenzaehler setzen
 		ZeilenCount = count;
+		//Rueckgabe des Arrays
 		return ausgabe;
 		
 	}
