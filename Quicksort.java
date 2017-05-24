9ff28f8c101b64e1ad1f1d39fb458394b1027ad2
tstelzle <tarek.stelzle@tu-dortmund.de>
2017-05-24 15:51:35 +0200

    Aufgabe 5.1 - approved

--- Quicksort.java
+++ Quicksort.java
@@ -41,23 +41,56 @@ public class Quicksort
 		//Feld wird initiliasiert, die Methode genRandom befuell das Array
 		int[] arr = genRandom(arrSize);
 		
-		//Pruefung ob das Array schon sortiert ist
-		System.out.println(isSorted(arr));
-		
-		//Variableninitialisierung fuer die Zeitmessung
+		int[] quick = copyArray(arr);
+		zeit("Quick", quick);
+		assert isSorted(quick);
+		int[] insertion = copyArray(arr);
+		zeit("Insertion", insertion);
+		assert isSorted(insertion);
+		int[] bubble = copyArray(arr);
+		zeit("Bubble", bubble);
+		assert isSorted(bubble);
+		int[] merge = copyArray(arr);
+		zeit("Merge", merge);
+		assert isSorted(merge);
+	}	
+
+	public static void zeit(String algorithm, int[] arr)
+	{
 		long tStart, tEnd;
-		
-		//Zeitmessung starten
-		tStart = System.currentTimeMillis();
-		//Quicksort wird ausgefuhrt
-		quicksort(arr, 0, arr.length-1);
-		//Zeitmessung stoppen
-		tEnd = System.currentTimeMillis();
-		
-		System.out.println("Dauer: " + (tEnd-tStart) + " ms");
-		
-		//Pruefung ob das Array aufsteigen sortiert wurde
-		System.out.println(isSorted(arr));
+		if(algorithm.equals("Quick"))
+		{
+			tStart = System.currentTimeMillis();
+			quicksort(arr, 0, arr.length-1);
+			tEnd = System.currentTimeMillis();
+			System.out.println("Quicksortdauer: " + (tEnd-tStart) + " ms");
+			System.out.println("Sorted? " + isSorted(arr));
+		}
+		else if(algorithm.equals("Insertion"))
+		{
+			tStart = System.currentTimeMillis();
+			insertionSort(arr);
+			tEnd = System.currentTimeMillis();
+			System.out.println("Insertionsortdauer: " + (tEnd-tStart) + " ms");
+			System.out.println("Sorted? " + isSorted(arr));
+		}
+		else if(algorithm.equals("Bubble"))
+		{
+			tStart = System.currentTimeMillis();
+			bubbleSort(arr);
+			tEnd = System.currentTimeMillis();
+			System.out.println("Bubblesortdauer: " + (tEnd-tStart) + " ms");
+			System.out.println("Sorted? " + isSorted(arr));
+		}
+		else if(algorithm.equals("Merge"))
+		{
+			tStart = System.currentTimeMillis();
+			mergeSort(arr);
+			tEnd = System.currentTimeMillis();
+			System.out.println("Mergesortdauer: " + (tEnd-tStart) + " ms");
+			System.out.println("Sorted? " + isSorted(arr));
+		}
+
 	}
 
 	//Methode zur Pruefung ob ein Array aufsteigend sortiert ist
@@ -72,6 +105,14 @@ public class Quicksort
 		}
 
 		return true;
+	}
+
+	static int[] copyArray(int[] arr)
+	{
+		int[] tmp = new int[arr.length];
+		for(int i=0; i<arr.length; i++)
+			tmp[i] = arr[i];
+		return tmp;
 	}
 	
 	//Quicksortalgorithmus nach Aufgabenstellung
@@ -103,10 +144,91 @@ public class Quicksort
 					j = j-1;
 				}
 			}
+			//FEHLT
+			//checkt ob die linke Teiliste nur kleinere Element als pivot beinhaltet, analog fuer die rechte Haelfte
 		quicksort(arr, l, j);
 		quicksort(arr, i ,r);
 		}
 	}
+	
+	public static void insertionSort(int []array)
+    	{
+        	for(int j = 1; j < array.length; j++) {
+            	int key = array[j];
+            	int i = j-1;
+	        while((i >= 0) && array[i] > key)
+		{
+               		array[i+1] = array[i];
+             		i--;
+            	}
+           	 array[i+1] = key;
+        	}
+        	assert isSorted(array) : "insertionSort hat die Liste nicht sortiert :/";
+    	}
+	
+	//Algorithmus Bubblesort wie im Pseudocode vorgegeben
+	public static void bubbleSort(int[] array)
+	{
+		int n = array.length;
+		for(int i=0; i < n; i++)
+		{
+			for(int j=n-1; j>i; j--)
+			{
+				if(array[j-1] > array[j])
+				{
+					int tmp = array[j];
+					array[j] = array[j-1];
+					array[j-1] = tmp;
+				}
+			}	
+		}
+	}
+
+	    public static void mergeSort(int []array)
+	    {
+		int[] tmpArray = new int[array.length];
+		mergeSort(array, tmpArray, 0, array.length-1);
+		assert isSorted(array) : "mergeSort hat die Liste nicht sortiert :/";
+	    }
+
+	    public static void mergeSort(int[] array, int[] tmpArray, int left, int right)
+	    {
+		if(left < right) {
+		    int q = (left+right)/2;
+		    mergeSort(array, tmpArray, left, q);
+		    mergeSort(array, tmpArray, q+1, right);
+		    merge(array, tmpArray, left, right);
+		}
+	    }
+
+	    public static void merge(int[] array, int[] tmpArray, int left, int right)
+	    {
+		int mid = (left+right+2)/2;
+		int pos1 = left;
+		int pos2 = mid;
+		for(int i=left; i <= right; i++) {
+		    if(pos1 >= mid) {
+			tmpArray[i] = array[pos2];
+			pos2++;
+			continue;
+		    }
+		    if(pos2 > right) {
+			tmpArray[i] = array[pos1];
+			pos1++;
+			continue;
+		    }
+		    if(array[pos1] <= array[pos2]) {
+			tmpArray[i] = array[pos1];
+			pos1++;
+			continue;
+		    }
+		    tmpArray[i] = array[pos2];
+		    pos2++;
+		}
+		for(int i=left; i <= right; i++) {
+		    array[i] = tmpArray[i];
+		}
+	     }
 	
 	//Methode befuellt ein Array mit zufaelligen Zahlen, die Arraygroesse wird als Parameter uebergeben
 	public static int[] genRandom(int size)
