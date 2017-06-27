6981d3c7e7c479f3e4402fe06c34803ef68f9510
tstelzle <tarek.stelzle@tu-dortmund.de>
2017-06-27 16:19:41 +0200

    Aufgabe 10.1

--- Subarray.java
+++ Subarray.java
@@ -45,6 +45,10 @@ public class Subarray {
 		printArr(neg);
 		printArr(both);
 
+		notNaiv(pos);
+		notNaiv(neg);
+		notNaiv(both);
+
 		zeitMessung(pos);
 		zeitMessung(neg);
 		zeitMessung(both);
@@ -73,11 +77,85 @@ public class Subarray {
 		long tEnd = System.currentTimeMillis();
 		System.out.println(", Dauer: " + (tEnd - tStart));
 	}
+	
+	public static void notNaiv(int[] arr)
+	{
+		int n = arr.length;
+		int[] links = new int[n+1];
+		int[] rechts = new int[n+1];
+		int[] summe = new int[n+1];
+
+		links[0] = 0;
+		rechts[0] = 0;
+		summe[0] = 0;
+
+		for(int i=1; i<n+1; i++)
+		{
+			if(summe[i-1] == 0)
+			{
+				if(arr[i-1] > 0)
+				{
+					links[i] = i-1;
+					rechts[i] = i-1;
+					summe[i] = arr[i-1];
+				}
+				else{
+					summe[i] = 0;
+					links[i] = i;
+					rechts[i] = i;
+				}
+			}
+			else if(summe[i-1] + arr[i-1] < arr[i-1])
+			{
+				links[i] = i-1;
+				rechts[i] = i-1;
+				summe[i] = arr[i-1];
+			}
+			else
+			{
+				links[i] = links[i-1];
+				rechts[i] = i-1;
+				summe[i] = summe[i-1] + arr[i-1];
+			}
+		}
+
+		int max = summe[0];
+		int pos = 0;
+
+		for(int i=1; i<summe.length; i++)
+		{
+			if(summe[i] > max)
+			{
+				max = summe[i];
+				pos = i;
+			}
+		}
+		
+		if(max == 0)
+		{
+			max = arr[0];
+			pos = 0;
+			for(int i=1; i<arr.length; i++)
+			{
+				if(max < arr[i])
+				{
+					max = arr[i];
+					pos = i;
+				}	
+			}
+		}
+		
+		System.out.println("Linke Grenze: " + links[pos] + ", Rechte Grenze: " + rechts[pos]  + ", Gesamtsumme: " + max);
+	}
 
 	public static void naiv(int[] arr)
 	{
+		int n = arr.length +1;
+		int[][] summe = new int[n][n];
+		summe[0][0] = 0;
+		int[] links = new int[n];
+		int[] rechts = new int[n];
 		int sum = 0;
-		int sum2 = 0;
 		int s = 0;
 		int e = arr.length -1;
 		boolean pos = true;
@@ -107,16 +185,20 @@ public class Subarray {
 		if(pos)
 		{
 			sum  = arr[0];
+			e = 0;
 
 			for(int i = 1; i<arr.length; i++)
 			{
 				if(sum < arr[i])
 				{
 					sum = arr[i];
+					s = e = i;
 				}
 			}
 		}
 		
+
+		// TODO Fehler, wenn in arr "both" die ersten 2 Werte negativ sind und der letzte positive gibt er was falsches aus
 		if(pos == false && neg == false)
 		{
 			sum = 0;
@@ -124,18 +206,46 @@ public class Subarray {
 			{
 				for(int j=0; j<arr.length; j++)
 				{		
-					sum2 = 0;
-
-					for(int x=i; x<= j; x++)
+					if(j == 0)
 					{
-						sum2 += arr[x];
+						if(arr[j] > 0)
+						{
+							summe[i][j] = arr[j];	
+							links[i] = j;
+							rechts[j] = j;
+						}
+						else
+						{
+							summe[i][j] = 0;
+							links[i] = j;
+							rechts[j] = j;
+						}
 					}
-
-					if(sum2 > sum)
+					else
 					{
-						s = i;
-						e = j;
-						sum = sum2;
+						summe[i][j]  = summe[i][j-1] + arr[j];
+						if(i == 0)
+						{
+							links[i] = 0;
+						}
+						else
+						{
+							links[i] = links[i-1];
+						}
+						rechts[j] = rechts[j-1] + 1;
+					}
+				}
+			}
+			
+			for(int i=0; i<summe.length; i++)
+			{
+				for(int j=0; j<summe[i].length; j++)
+				{
+					if(sum < summe[i][j])
+					{
+						sum = summe[i][j];
+						s = links[i]; 
+						e = rechts[j];
 					}
 				}
 			}
@@ -143,21 +253,4 @@ public class Subarray {
 
 		System.out.print("Linke Grenze: " + s + ", Rechte Grenze: " + e + ", Gesamtsumme: " + sum);
 	}
-
-	public static int[] algorithm(int[] arr)
-	{
-		if(arr.length == 1)
-		{
-			if(arr[0] < 0)
-			{
-				// TODO return [0,1];
-			}
-		}
-		for(int j=0; j<arr.length; j++)
-		{
-		
-		}
-		return null;
-	}
-
 }
