4e60114ad49ae737fbcd238787a423fc867335b1
tstelzle <tstelzle@online.de>
2017-07-01 11:52:12 +0200

    Add files via upload
    Aufgabe 11.1

---
+++ Heap.java
@@ -0,0 +1,42 @@
+public class Heap {
+	
+	public int[] heap;
+	public int heapSize;
+	
+	public Heap(int n)
+	{
+		heapSize = n;
+		heap = new int[heapSize];
+	}
+	
+	public int left(int i)
+	{
+		
+	}
+	
+	public int parent(int i)
+	{
+		
+	}
+	
+	public int right(int i)
+	{
+		
+	}
+	
+	public void insert(int key)
+	{
+		
+	}
+	
+	public int extractMax()
+	{
+		
+	}
+	
+	public void printHeap()
+	{
+		
+	}
+
+}
