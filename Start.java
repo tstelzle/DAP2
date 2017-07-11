3689177888c26b1fbb64a489a371d8f71a04fc5f
Tarek Stelzle <tstelzle@online.de>
2017-07-11 19:52:15 +0200

    Aufgabe 12 - Start

--- Start.java
+++ Start.java
@@ -2,6 +2,12 @@ public class Start
 {
 	public static void main(String[] args)
 	{
+		if(args.length != 1 && args.length != 3)
+		{
+			System.out.println("Falsche Argumentanzahl.");
+			System.exit(0);
+		}
+		
 		String filePath = "";
 
 		try{
@@ -14,21 +20,37 @@ public class Start
 		}
 		Graph g = new Graph();
 		g = g.fromFile(filePath);
-		System.out.println(g.toString());
-
-		int startknoten = 0;
-
-		int[] ausgabe = g.bfs(g, startknoten);
-		for(int i=0; i<ausgabe.length; i++)
+		if(args.length == 1)
 		{
-			System.out.println("Abstand von: " + startknoten + " zu " + i + " ist: " + ausgabe[i]);
+			System.out.println(g.toString());
 		}
-		/*
-		char[] arr = g.getNodes();
-		for(int i=0; i<arr.length; i++)
+
+		if(args.length == 3)
 		{
-			System.out.println(i + " " + arr[i]);
+			String algo = "";
+			int startknoten = 0;
+
+			try{
+				algo = args[1];
+				startknoten = Integer.parseInt(args[2]);
+			}
+			catch(Exception e)
+			{
+				System.out.println("Argument konnte nicht eingelesen werden.");
+				System.exit(0);
+			}
+
+			if(!algo.equals("bfs"))
+			{
+				System.out.println("Algorithmus nicht bekannt.");
+				System.exit(0);
+			}
+
+			int[] ausgabe = g.bfs(g, startknoten);
+			for(int i=0; i<ausgabe.length; i++)
+			{
+				System.out.println("Abstand von: " + startknoten + " zu " + i + " ist: " + ausgabe[i]);
+			}
 		}
-		*/
 	}
 }
